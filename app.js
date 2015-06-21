93b9dc9669cc9936af7de768b98a82ea6d88560b
Nathan Totten <ntotten@auth0.com>
2015-06-21 14:56:54 -0700

    Zendesk webhook

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
+require("babel/register");
+require('dotenv').load();
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
-var routes = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
 
@@ -22,14 +21,13 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+app.use('/webhooks', require('./lib/webhooks-routes'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
 // error handlers
@@ -37,23 +35,23 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.json({
+      message: err.message,
+      error: {}
     });
+  });
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
+  res.status(err.status || 500);
+  res.json({
+    message: err.message,
+    error: {}
+  });
 });
 
 
---
+++ test/webhooks-tests.js
@@ -0,0 +1,19 @@
+require('dotenv').load();
+var assert = require('assert');
+var commands = require('../lib/commands');
+
+describe('Ticket command', function(){
+  it('should work', function(done){
+    this.timeout(10000);
+    var data = {
+      token: process.env.SLACK_COMMAND_TOKEN,
+      user_id: process.env.TEST_SLACK_USER_ID,
+      text: '@ntotten This is a test test test test'
+    }
+    commands.openTicket(data)
+    .then(result => {
+      console.log(result);
+      done();
+    }).catch(done);
+  });
+})
