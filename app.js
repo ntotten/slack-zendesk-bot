4992202b3db53d6d48d20e7fbc483543fa42756d
Nathan Totten <ntotten@auth0.com>
2015-06-23 08:48:25 -0700

    Removed unused files

--- app.js
+++ app.js
@@ -18,8 +18,6 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/webhooks', require('./lib/webhooks-routes'));
 
