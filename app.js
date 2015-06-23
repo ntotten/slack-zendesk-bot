029e960c19a5ac2a55c40d572e56bb6ed5372b20
Nathan Totten <ntotten@auth0.com>
2015-06-23 09:38:29 -0700

    renamed file

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/webhooks', require('./lib/webhooks-routes'));
+app.use('/webhooks', require('./lib/routes'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
